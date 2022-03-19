8a808dc8fea621eacba159bfb273f7afd2d416f0
Tyler Hunt <tyler.s.hunt05@gmail.com>
2022-03-19 10:33:34 -0600

    binary tree 101

---
+++ leetcode/invert-binary-tree/solution.py
@@ -0,0 +1,35 @@
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # recursively
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+
+    # dfs (stack)
+    def invertTreeDFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.left)
+                stack.append(node.right)
+
+    def invertTreeDFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        queue = collections.deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.right)
+                queue.append(node.left)
